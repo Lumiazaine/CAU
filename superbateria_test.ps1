181592cabe7f09523b92727ba05563b682168577
JUST3EXT <133763083+JUST3EXT@users.noreply.github.com>
2023-05-16 15:03:09 +0200

    Update superbateria_test.ps1

--- superbateria_test.ps1
+++ superbateria_test.ps1
@@ -1,5 +1,5 @@
 # ¡IMPORTANTE!  Si no funciona, ejecutra - Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope LocalMachine
-#Prueba de update v0.8 Beta
+#Prueba de update v0.10 Beta
 
 
 # Verificar si el script se está ejecutando con permisos de administrador
@@ -214,18 +214,28 @@ function EjecutarOpcion {
             }
             elseif ($osVersion -like "*Windows 10*") {
                  #Actualizar script Windows 10
-            $scriptUrl = "https://raw.githubusercontent.com/JUST3EXT/CAU/main/superbateria_test.ps1"
-            $localScriptPath = $scriptDirectory
-
-            # Descargar el script actualizado desde la URL de GitHub
-            Invoke-WebRequest -Uri $scriptUrl -OutFile $localScriptPath -ErrorAction Stop
-
-            # Verificar si la descarga fue exitosa
-            if (Test-Path $localScriptPath) {
-                Write-Host "El script se ha actualizado correctamente."
-            } else {
-                Write-Host "No se pudo descargar el script actualizado desde la URL de GitHub."
-            }
+                 $scriptUrl = "https://raw.githubusercontent.com/JUST3EXT/CAU/main/superbateria_test.ps1"
+                 $localScriptPath = $scriptDirectory
+                 
+                 # Verificar si se están ejecutando con privilegios de administrador
+                 $isAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+                 
+                 if ($isAdmin) {
+                     # Descargar el script actualizado desde la URL de GitHub
+                     $webClient = New-Object System.Net.WebClient
+                     $webClient.DownloadFile($scriptUrl, $localScriptPath)
+                 
+                     # Verificar si la descarga fue exitosa
+                     if (Test-Path $localScriptPath) {
+                         Write-Host "El script se ha actualizado correctamente."
+                     } else {
+                         Write-Host "No se pudo descargar el script actualizado desde la URL de GitHub."
+                     }
+                 } else {
+                     # Solicitar permisos de administrador y volver a ejecutar el script
+                     Start-Process powershell.exe -Verb RunAs -ArgumentList "-File `"$PSCommandPath`""
+                     Exit
+                 }
             }
             else {
 
