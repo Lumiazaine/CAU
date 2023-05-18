f6bee2f867a5a004253fad76454ca083a7cd8dc9
JUST3EXT <133763083+JUST3EXT@users.noreply.github.com>
2023-05-18 08:43:33 +0200

    Bateria de pruebas CAU 2012

---
+++ bateria_pruebas.bat
@@ -0,0 +1,31 @@
+﻿
+ipconfig /flushdns
+
+
+@echo off
+cmdkey /list:>previa.txt
+
+
+del /q/f/s %TEMP%\*
+
+javac -cache-dir c:\temp\jws
+javaws -clearcache
+javaws -Xclearcache -silent -Xnosplash
+
+
+RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 8
+RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 2
+RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 1
+
+
+rd /s /q %systemdrive%\$Recycle.bin 
+
+
+
+@echo off
+cmdkey /list:>post.txt
+
+fc previa.txt post.txt
+
+gpupdate /force 
+
