ec48f0c14c65272ed500bd7771de1faba350b3ea
Lumiazaine <lumiazaine@gmail.com>
2025-03-28 13:27:03 +0100

    Se modifica mensaje error

--- Scripts/CAUJUS.bat
+++ Scripts/CAUJUS.bat
@@ -2,7 +2,7 @@
 for /f "tokens=*" %%A in ('hostname') do set "hostname=%%A"
 if "%hostname%"=="IUSSWRDPCAU02" (
     cls
-    echo Las palabras del CAU01 resuenan en tu cabeza ahora mismo: "los script en la maquina de salto no."
+    echo Error, se está ejecutando el script desde la máquina de salto.
     pause
     exit
 ) else (
